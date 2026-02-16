9724a4bd5eb6d8bf110c5ce7ba034d353f2b0535
Ilia Turygin <159892171+iliatur@users.noreply.github.com>
2026-02-16 09:14:51 +0300

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = []
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
