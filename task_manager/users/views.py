6bab89a7297360cb3ab9cb0f1113f15e3a42702b
Ilia Turygin <turygin_ia@mail.ru>
2026-02-16 13:07:07 +0300

    Add compiled translations

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
@@ -18,25 +18,49 @@ from task_manager.users.forms import (
 User = get_user_model()
 
 
+# 🔹 Миксин для обработки отсутствия прав
+class PermissionDeniedMixin:
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                _("You are not logged in! Please log in.")
+            )
+            return redirect(
+                f"{reverse_lazy('login')}?next={self.request.path}"
+            )
+
+        messages.error(
+            self.request,
+            _("You do not have permission to perform this action.")
+        )
+        return redirect('users_index')
+
+
+# 🔹 Список пользователей
 class UsersIndexView(ListView):
     model = User
     template_name = 'users/index.html'
     context_object_name = 'users'
 
 
+# 🔹 Регистрация пользователя
 class UserCreateView(SuccessMessageMixin, CreateView):
+    model = User
     form_class = UserRegistrationForm
     template_name = 'users/create.html'
     success_url = reverse_lazy('login')
     success_message = _('User created successfully')
 
 
+# 🔹 Обновление пользователя
 class UserUpdateView(
+    PermissionDeniedMixin,
     LoginRequiredMixin,
     UserPassesTestMixin,
     SuccessMessageMixin,
     UpdateView
-    ):
+):
     model = User
     form_class = UserUpdateForm
     template_name = 'users/update.html'
@@ -46,26 +70,20 @@ class UserUpdateView(
     def test_func(self):
         return self.request.user == self.get_object()
 
-    def handle_no_permission(self):
-        if not self.request.user.is_authenticated:
-            messages.error(
-                self.request,
-                _("You are not logged in! Please log in.")
-                )
-            return super(LoginRequiredMixin, self).handle_no_permission()
-        messages.error(
-            self.request,
-            _("You do not have permission to perform this action.")
-            )
-        return redirect('users_index')
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        update_session_auth_hash(self.request, self.object)
+        return response
 
 
+# 🔹 Удаление пользователя
 class UserDeleteView(
+    PermissionDeniedMixin,
     LoginRequiredMixin,
     UserPassesTestMixin,
     SuccessMessageMixin,
     DeleteView
-    ):
+):
     model = User
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users_index')
@@ -73,19 +91,6 @@ class UserDeleteView(
 
     def test_func(self):
         return self.request.user == self.get_object()
-    
-    def handle_no_permission(self):
-        if not self.request.user.is_authenticated:
-            messages.error(
-                self.request,
-                _("You are not logged in! Please log in.")
-                )
-            return super(LoginRequiredMixin, self).handle_no_permission()
-        messages.error(
-            self.request,
-            _("You do not have permission to perform this action.")
-            )
-        return redirect('users_index')
 
     def post(self, request, *args, **kwargs):
         try:
@@ -93,21 +98,22 @@ class UserDeleteView(
         except ProtectedError:
             messages.error(
                 request,
-                _("It is impossible to delete the user \
-                    because it is being used")
+                _("It is impossible to delete the user because it is being used")
             )
             return redirect(self.success_url)
 
 
+# 🔹 Логин
 class UserLoginView(SuccessMessageMixin, LoginView):
     form_class = UserLoginForm
     template_name = 'users/login.html'
     success_message = _("You are logged in")
-    
+
     def get_success_url(self):
         return reverse_lazy('index')
 
 
+# 🔹 Логаут
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('index')
 
